c4620d08e26708ce4bcdfc8fb25426794356253d
manikantmaurya <64607600+manikantmaurya@users.noreply.github.com>
2022-03-22 01:01:11 +0530

    Add files via upload

---
+++ Project Mandalorian V2/animation.js
@@ -0,0 +1,21 @@
+$("#button").click(function() {
+    $('.mandalorian-section-2').toggleClass('mandalorian-section-2-animated');
+    $('.yoda-title').toggleClass('yoda-title-animated');
+    $('.yoda-white-bar').toggleClass('yoda-white-bar-animated');
+    $('.baby_yoda-section-2').toggleClass('baby_yoda-section-2-animated');
+    $('.yoda-focus-img').toggleClass('yoda-focus-img-animated');
+    $('.yoda-focus-container').toggleClass('yoda-focus-container-animated');
+    $('.white-bar-container').toggleClass('white-bar-container-animated');
+    $('.yoda-bg-img').toggleClass('rotate-animated');
+    $('.yoda-subtitle').toggleClass('rotate-animated');
+    $('.yoda-title-bar').toggleClass('yoda-title-bar-animated');
+    $('.mandalorian-title').toggleClass('mandalorian-title-animated');
+    $('.mandalorian-the').toggleClass('mandalorian-the-animated');
+    $('.boba_action-img').toggleClass('boba_action-img-animated');
+    $('.boba_focus-img').toggleClass('boba_focus-img-animated');
+    $('.divider').toggleClass('divider-animated');
+    $('.mandalorian-desc').toggleClass('mandalorian-desc-animated');
+    $('body').toggleClass('body-background');
+    });
+
+
